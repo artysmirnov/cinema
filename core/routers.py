777a68f9eff3db02bf22e0d56fdc94c353d73d6c
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-24 15:26:38 +0300

    add refresh api

--- core/routers.py
+++ core/routers.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from core.auth.viewsets import RegisterViewSet, LoginViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 from core.user.viewsets import UserViewSet
 
 
@@ -11,6 +11,7 @@ router.register(r'user', UserViewSet, basename='user')
 
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 
 urlpatterns = [
