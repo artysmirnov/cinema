f1081987cb04a6f0cf1301040a500c4cf049416b
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-05-14 14:50:16 +0300

    add admin functonality to person

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from core.film.admin import FilmAdmin
 from core.film.models import Film
+from core.person.admin import PersonAdmin
 from core.person.models import Person
 
 admin.site.register(Film, FilmAdmin)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
--- core/person/admin.py
+++ core/person/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.person.models import Person
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name']
+
+    class Meta:
+        model = Person
