85ff5b5e8fe0ed6f45207c8b5f2142856f86c0d5
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-05-16 11:25:00 +0300

    deleted app rating from admin

--- core/admin.py
+++ core/admin.py
@@ -4,8 +4,6 @@ from core.film.admin import FilmAdmin
 from core.film.models import Film
 from core.person.admin import PersonAdmin
 from core.person.models import Person
-from core.score.models import Score
 
 admin.site.register(Film, FilmAdmin)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Score)
