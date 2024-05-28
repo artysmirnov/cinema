77b85e330096adc3ee2d2c6a298215eaa0141ad8
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-05-28 20:56:07 +0300

    create base login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
-	return <div>Login here</div>
+	return (
+		<div className="login">
+			<form action="">
+				<h2>Login</h2>
+				<p>Enter email</p>
+				<input type="text" placeholder="email" />
+				<p>Enter password</p>
+				<input type="password" placeholder="password" />
+				<input type="submit" />
+			</form>
+			<Link to={"/register"}>Don`t have account</Link>
+		</div>
+	)
 }
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom"
+
 const Register = () => {
-	return <div>Registration here</div>
+	return (
+		<div className="registration">
+			<form action="">
+				<h2>Registration</h2>
+				<p>Enter email</p>
+				<input type="text" placeholder="email" />
+				<p>Enter password</p>
+				<input type="password" placeholder="password" />
+				<p>Enter password again</p>
+				<input type="password" placeholder="password" />
+				<input type="submit" />
+			</form>
+			<Link to={"/login"}>Already have account</Link>
+		</div>
+	)
 }
 
 export default Register
