28928babb5876347335ff7a0da8004d0c6e93274
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-05-31 17:01:42 +0300

    films on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react"
+import SingleFilm from "../components/SingleFilm"
 const Home = () => {
-	return <div>Home is here</div>
+	const [films, setFilms] = useState([])
+	const [loading, setLoading] = useState(true)
+	async function fetchFilms(set) {
+		try {
+			const data = await fetch("http://127.0.0.1:8000/api/film/")
+			const results = await data.json()
+			set(results.results)
+			setLoading(false)
+		} catch (e) {
+			console.log(e)
+		}
+	}
+	useEffect(() => {
+		fetchFilms(setFilms)
+	}, [])
+	console.log(films[0])
+	return (
+		<div>
+			<p>Home is here</p>
+			<div className="films-container">
+				{loading ? (
+					<p>loading...</p>
+				) : (
+					films.map(film => {
+						;<SingleFilm key={film.id} results={film} />
+					})
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Home
